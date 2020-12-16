157e422eace6f185fb95a8b34372154d4516f76a
Delta9k8b <keaton.brewster@gmail.com>
2020-12-16 06:14:47 -0600

    major function cleanup. cleaned up the generatePassword more by adding another function, to reduce the number of repeated lines of code.

--- script.js
+++ script.js
@@ -25,6 +25,7 @@ function writePassword() {
         passwordText.value = password;
       }
     }
+    // if you did not accept any password criteria, you end up here.
     else {
       alert("You must chose at least one of the password criteria")
     }
@@ -41,32 +42,27 @@ function writePassword() {
     let randomSet = [];
     let ensureSet = [];
     let passwordString = [];
-
+    // AND the variables for the contents of each possible character set. 
     var upperRange = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     var lowerRange = "abcdefghijklmnopqrstuvwxyz";
     var specialRange = "!@#$%^&*()_+~`-=:;',.<>?/{}[]";
     var numberRange = "1234567890";
-
-    // doing truth checks to print the according char-sets into the respective arrays
-    if (upperCase === true) {
-      randomSelector(upperRange);
-      charsetEnsure(upperRange);
+    // doing truth checks to make sure the character sets are only included if the user selected them, then running the functions to include them if they pass.
+    truthCheck(upperCase, upperRange);
+    truthCheck(lowerCase, lowerRange);
+    truthCheck(specialCase, specialRange);
+    truthCheck(numbers, numberRange);
+    // Truth-check function for the character selection. These are the functions run if the char-sets pass the truth test
+    function truthCheck(x,y) {
+      if (x) {
+        randomSelector(y);
+        charsetEnsure(y);
+      };
     };
-    if (lowerCase === true) {
-      randomSelector(lowerRange);
-      charsetEnsure(lowerRange);
-    };
-    if (specialCase === true) {
-      randomSelector(specialRange);
-      charsetEnsure(specialRange);
-    };
-    if (numbers === true) {
-      randomSelector(numberRange);
-      charsetEnsure(numberRange);
-    };
-    // this is the function that provides a bunch of random characters to chose from, and sends them into the randomSet array
-    // a lot of different methods could be used for the for-loop, specifically the middle parameter. I chose i < 75 so that it would really fill up the array with options
-    // to choose from, to ensure a thoroughly random password, without running too many iterations and causing the function to run really slow. 
+    // this is the function that provides a bunch of random characters to chose from, and sends them into the randomSet array, to later be chosen from randomly agin.
+    // There are different methods that could be used in place of the for-loop, specifically the middle parameter. 
+    // I chose i < 75 so that it would really fill up the randomSet array with options to choose from, to ensure a thoroughly random password, 
+    // without running too many iterations and causing the function to run really slow. 
     function randomSelector(x) {
       for (let i = 0; i < 75; i++) {
         xChar = x.charAt(Math.floor(Math.random() * x.length));
@@ -78,12 +74,11 @@ function writePassword() {
       xChar = x.charAt(Math.floor(Math.random() * x.length));
       ensureSet.push(xChar);
     };
-    //push the ensured chars to the passwordString, and joining them together so that they don't print with commas later.
+    //push the ensured chars to the passwordString, while joining them together so that they don't print with commas later.
     passwordString.push(ensureSet.join(''));
-
-    // this loop picks random characters form the list complied by the previous loop 
-    // it uses passwordLength - the length of ensureSet to ensure the generated password is the same length as specified by the user
-    // we have to subtract the length of ensureSet to still get the right number of characters upon generation.
+    // this loop picks random characters form the list complied by the previous loop (randomSelector())
+    // it uses (passwordLength - ensureSet.length) to ensure the generated password is the same length as specified by the user
+    // (you must subtract ensureSet, because those characters will be added by force, and do not account for what the user specified the length to be)
     for (let i = 0; i < (passwordLength - ensureSet.length); i++) {
       var charPicked = randomSet[Math.floor(Math.random() * randomSet.length)];
       passwordString.push(charPicked);
@@ -92,8 +87,8 @@ function writePassword() {
     // so that the main function can access the randomly generated password. 
     console.log(passwordString); //console check
     return passwordString.join('').toString();
-  }
-}
+  };
+};
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
