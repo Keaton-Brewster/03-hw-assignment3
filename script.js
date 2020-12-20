3cb1996975d0fd5c6bf073fb9964403ef6ffeffa
Delta9k8b <keaton.brewster@gmail.com>
2020-12-20 11:38:51 -0600

    reverted to the application method using checkboxes and value sliders to forgo the use of JS prompts and JS confirms

--- script.js
+++ script.js
@@ -1,38 +1,85 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate"),
+    resetBtn = document.querySelector("#resetBtn"),
+    passwordText = document.querySelector("#password");
+
+
+// made a few global variables, since this is a simple application and it will make a couple things easier. 
+var passwordLength;
+// These are the booleans for the checkboxes
+var upperCase;
+var lowerCase;
+var specialCase;
+var numbers;
+
+// setting up the value slider.
+// this is NOT a part of the assignment, but i feel that it greatly improves the user experience, so why not?
+var slider = document.getElementById("passwordLength");
+var output = document.getElementById("passwordValue");
+output.innerHTML = slider.value;
+slider.oninput = function () {
+  output.innerHTML = this.value;
+};
+
+// I chose to use checkboxes in the html for selecting password criteria instead of prompts, 
+// Like the slider, I know the assignment called for something else, but i felt that checkboxes were more use friendly. 
+// This section of the code simply checks the checkboxes and sets a true/false statement depending on their position. 
+document.getElementById('upperCase').onclick = function () {
+  if (this.checked) {
+    upperCase = true;
+  }
+  else {
+    upperCase = false;
+  }
+  console.log(upperCase)//console check
+};
+document.getElementById('lowerCase').onclick = function () {
+  if (this.checked) {
+    lowerCase = true;
+  }
+  else {
+    lowerCase = false;
+  }
+  console.log(lowerCase)//console check
+};
+document.getElementById('specialChar').onclick = function () {
+  if (this.checked) {
+    specialCase = true;
+  }
+  else {
+    specialCase = false;
+  }
+  console.log(specialCase)//console check
+};
+document.getElementById('numbers').onclick = function () {
+  if (this.checked) {
+    numbers = true;
+  }
+  else {
+    numbers = false;
+  }
+  console.log(numbers)//console check
+};
+
 
 // Write password to the #password input
 function generatePassword() {
+  // if/else to check and make sure at least one of the criteria had been selected. 
+  if (upperCase || lowerCase || specialCase || numbers) {
 
-  // Using while loop to get user input on how long the password should be
-  do {
-    passwordLength = prompt("Please pick a password length between 8 and 128 characters long.");
+    passwordLength = document.getElementById("passwordLength").value;
+    var password = generateRandomPassword();
+    if (password === undefined) { // give an alert if the password generator fails for some reason. for dev purposes
+      alert("error");
+    }
+    else {
+      passwordText.value = password;
+    }
   }
-  // In this while statement, i specify that passwordLength must be a number, and must be between 8 and 128. 
-  // if the user given criteria for the password length does not meet these conditions, the prompt will be repeated. 
-  while (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128);
-  alert("You password will be " + passwordLength + " characters long."); // Confirming what they chose.
-
-  // Once the user has satisfied the criteria for passwordLength, they are asked to confirm which character sets they want to include in their password. 
-  upperCase = confirm("Would you like your password to include UPPER-CASE characters? (OK=YES, CANCEL=NO)");
-  lowerCase = confirm("Would you like your password to include LOWER-CASE characters? (OK=YES, CANCEL=NO)");
-  specialCase = confirm("Would you like your password to include SPECIAL characters? (OK=YES, CANCEL=NO)");
-  numbers = confirm("Would you like your password to include NUMBERS? (OK=YES, CANCEL=NO)");
-
-  // Check here to make sure at least one of the criteria had been selected. 
-  // If user selected no to all of them, they will be alerted, and the function quits. 
-  if (upperCase === false && lowerCase === false && specialCase === false && numbers === false) {
-    alert("You must select yes for at least one password criteria");
-    return;
+  // if user did not check at least one box, you will get this else statement
+  else {
+    alert("You must chose at least one of the password criteria")
   }
-  // Once the required criteria is met, the password will be generated, along the criteria given. 
-  var password = generateRandomPassword();
-  var passwordText = document.querySelector("#password");
-  if (password === undefined) { // give an alert and stop the program if the password generator fails for some reason.
-    alert("error");
-    return;
-  }
-  passwordText.value = password; // If you make it here, the program is working!
 }
 
 // Here is the function that actually generates a random password.
@@ -61,7 +108,6 @@ function generateRandomPassword() {
       charsetEnsure(y); // details below::
     };
   };
-
   // this is the function that provides a bunch of random characters to chose from, and sends them into the randomSet array, to later be chosen from randomly agin.
   // There are different methods that could be used in place of the for-loop, specifically the middle parameter. 
   // I chose i < 100 so that it would really fill up the randomSet array with options to choose from, to ensure a thoroughly random password, 
@@ -72,7 +118,6 @@ function generateRandomPassword() {
       randomSet.push(xChar);
     };
   };
-
   // This function is what ensures that if a char-set passes its truth check, at least one of that sets characters will show up in the generated password. 
   function charsetEnsure(x) {
     xChar = x.charAt(Math.floor(Math.random() * x.length));
@@ -96,5 +141,11 @@ function generateRandomPassword() {
   return passwordString.join('').toString();
 };
 
+// Lastly, adding a function to reset the password. 
+function resetPassword() {
+  passwordText.value = '';
+};
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", generatePassword);
+resetBtn.addEventListener("click", resetPassword);
